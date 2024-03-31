2772dc7b4bf2f9732b105adf945aa45d4209e6e1
Jan Gross <jan.gross.cze@gmail.com>
2024-03-31 15:32:20 +0200

    filtering by color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,32 @@ import { QueryResult } from "@upstash/vector";
 import type { Product as TProduct } from "@/db";
 import Products from "@/components/Products/Products";
 import ProductSkeleton from "@/components/Products/ProductSkeleton";
+import { Accordion } from "@radix-ui/react-accordion";
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
 const SORT_OPTIONS = [
   { name: "None", value: "none" },
   { name: "Price: Low to High", value: "price-asc" },
   { name: "Price: High to Low", value: "price-desc" },
+] as const;
+
+const COLOR_FILTERS = {
+  id: "color",
+  name: "Color",
+  options: [
+    { value: "white", label: "White" },
+    { value: "beige", label: "Beige" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+    { value: "purple", label: "Purple" },
+  ] as const,
+}
+
+const SUBCATEGORIES = [
+  { name: "T-Shirts", selected: true },
+  { name: "Hoodies", selected: false },
+  { name: "Sweatshirts", selected: false },
+  { name: "Accessories", selected: false },
 ] as const;
 
 const Home = () => {
@@ -78,7 +99,51 @@ const Home = () => {
 
       <section className="pb-24 pt-6">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
-          {/* <div></div> */}
+          {/* FILTERS */}
+          <div className="hidden lg:block">
+            <ul className="space-y-4 border-b border-gry-200 pb-6 text-sm font-medium text-gray-900">
+              {SUBCATEGORIES.map((sub) => (
+                <li
+                  key={sub.name}
+                  className={cn("cursor-pointer", {
+                    "text-gray-900": sub.selected,
+                    "text-gray-400": !sub.selected,
+                  })}
+                >
+                  <button 
+                    disabled={!sub.selected}
+                    className="disabled:curson-not-allowed disabled:opacity-55">
+                    {sub.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+              <Accordion type="multiple" className="animate-none">
+                  <AccordionItem value="color">
+                    <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-600">
+                      <span className="font-medium text-gray-900">Color</span>
+                    </AccordionTrigger>
+
+                    <AccordionContent className="py-3 text-sm text-gray-900">
+                      <ul className="space-y-2">
+                       {COLOR_FILTERS.options.map((option, optionIdx) => (
+                          <li key={option.value} className="flex items-center">
+                            <input 
+                              type="checkbox" 
+                              id={`color-${optionIdx}`} 
+                              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                            />
+                            <label htmlFor={`color-${optionIdx}`} className="ml-3 text-sm text-gray-900">
+                              {option.label}
+                            </label>
+                          </li>
+                       ))}
+                      </ul>
+                    </AccordionContent>
+                  </AccordionItem>
+              </Accordion>
+
+          </div>
 
           <ul className="lg:col-span-3 grid grid-cols-1 sm:gird-cols-2 md:grid-cols-3 gap-8">
             {!products
