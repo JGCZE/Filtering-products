8ae6b85e981d19e261d682db4c1d518e34f9dcbc
Jan Gross <jan.gross.cze@gmail.com>
2024-03-31 12:56:31 +0200

    Products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { useState } from "react";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent } from "../components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "../components/ui/dropdown-menu";
 import { ChevronDown, Filter } from "lucide-react";
 import { cn } from "../lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { QueryResult } from "@upstash/vector";
-import { Product } from "@/db";
+import type { Product as TProduct } from "@/db";
+import Products from "@/components/Products/Products";
+import ProductSkeleton from "@/components/Products/ProductSkeleton";
 
 const SORT_OPTIONS = [
   { name: "None", value: "none" },
@@ -19,19 +25,21 @@ const Home = () => {
     sort: "none",
   });
 
-  const {data : products} = useQuery({
+  const { data: products } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const { data } = await axios.post<QueryResult<Product>[]>(
-        'http://localhost:3000/api/products', 
-        { filter: {
-          sort: filter.sort,
-        } });
-        return data;
+      const { data } = await axios.post<QueryResult<TProduct>[]>(
+        "http://localhost:3000/api/products",
+        {
+          filter: {
+            sort: filter.sort,
+          },
+        }
+      );
+      return data;
     },
-  })
+  });
 
-  console.log(products);
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -46,27 +54,44 @@ const Home = () => {
               <ChevronDown className="w-5 h-5 ml-2 -mr-1 text-gray-400 group-hover:text-gray-500" />
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              { SORT_OPTIONS.map(option => (
-                <button 
-                  key={option.value} 
-                  className={cn('text-left w-full block px-4 py-2 text-sm', { 
-                    'text-gray-900 bg-gray-100': option.value === filter.sort,
-                    'text-gray-500' : option.value !== filter.sort
-                   })}
-                  onClick={() => setFilter((prev) => ({ ...prev, sort: option.value }))}
+              {SORT_OPTIONS.map((option) => (
+                <button
+                  key={option.value}
+                  className={cn("text-left w-full block px-4 py-2 text-sm", {
+                    "text-gray-900 bg-gray-100": option.value === filter.sort,
+                    "text-gray-500": option.value !== filter.sort,
+                  })}
+                  onClick={() =>
+                    setFilter((prev) => ({ ...prev, sort: option.value }))
+                  }
                 >
                   {option.name}
                 </button>
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
-        <button className="m-2 ml-4 p-2 text-gray-400 hover:bg-gray-500 sm:ml-6 lg:hidden">
-          <Filter className="h-5 w-5"/>
-        </button>
-
+          <button className="m-2 ml-4 p-2 text-gray-400 hover:bg-gray-500 sm:ml-6 lg:hidden">
+            <Filter className="h-5 w-5" />
+          </button>
         </div>
       </div>
+
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+          {/* <div></div> */}
+
+          <ul className="lg:col-span-3 grid grid-cols-1 sm:gird-cols-2 md:grid-cols-3 gap-8">
+            {!products
+              ? new Array(12)
+                  .fill(null)
+                  .map((_, i) => <ProductSkeleton key={i} />)
+              : products.map((prod) => (
+                  <Products key={prod.id} product={prod.metadata!} />
+                ))}
+          </ul>
+        </div>
+      </section>
     </main>
   );
-}
+};
 export default Home;
